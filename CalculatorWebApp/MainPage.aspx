7940d703b6c65d4598f21cb31643340a10e372e5
Hubert Kowalczyk <kowalczyk.hubert@gmail.com>
2019-10-12 11:23:09 +0200

    Metoda oblicz + historia

--- CalculatorWebApp/MainPage.aspx
+++ CalculatorWebApp/MainPage.aspx
@@ -9,6 +9,36 @@
 <body>
     <form id="form1" runat="server">
         <div>
+            Pierwsza liczba:<br />
+            <asp:TextBox ID="LiczbaPierwsza" runat="server"></asp:TextBox>
+            <br />
+            <br />
+            Druga liczba<br />
+            <asp:TextBox ID="LiczbaDruga" runat="server"></asp:TextBox>
+            <br />
+            <br />
+            <asp:DropDownList ID="Dzialanie" runat="server">
+                <asp:ListItem Value="0">Dodawanie</asp:ListItem>
+                <asp:ListItem Value="1">Odejmowanie</asp:ListItem>
+                <asp:ListItem Value="2">Mnożenie</asp:ListItem>
+                <asp:ListItem Value="3">Dzielenie</asp:ListItem>
+            </asp:DropDownList>
+            <br />
+            <br />
+            <asp:Button ID="Oblicz" runat="server" OnClick="Oblicz_Click" Text="Oblicz" Width="132px" />
+            <br />
+            <br />
+            <asp:Label ID="Wynik" runat="server"></asp:Label>
+            <br />
+            <br />
+            Historia operacji<br />
+            <asp:ListBox ID="Historia" runat="server" Width="151px"></asp:ListBox>
+            <br />
+            <br />
+            <asp:Button ID="Odwtorz" runat="server" Text="Odtwórz" />
+            <br />
+            <br />
+            <br />
         </div>
     </form>
 </body>
--- CalculatorWebApp/MainPage.aspx.cs
+++ CalculatorWebApp/MainPage.aspx.cs
@@ -13,5 +13,84 @@ namespace CalculatorWebApp
         {
 
         }
+        //metoda dodająca działania do historii
+        private void dodajDzialanie(string pierwsza, string druga, int dzialanie, string wynik)
+        {
+            string ukrytaWartosc = pierwsza + ";" + druga + ";" + dzialanie.ToString() + ";" + wynik;
+            string widocznaWartosc = pierwsza + " ";
+            if (dzialanie == 0) widocznaWartosc += "+ ";
+            if (dzialanie == 1) widocznaWartosc += "- ";
+            if (dzialanie == 2) widocznaWartosc += "* ";
+            if (dzialanie == 3) widocznaWartosc += "/ ";
+            widocznaWartosc += druga;
+
+            ListItem element = new ListItem(widocznaWartosc, ukrytaWartosc);
+            Historia.Items.Add(element);
+        }
+        protected void Oblicz_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                double num1 = Convert.ToDouble(LiczbaPierwsza.Text);
+                double num2 = Convert.ToDouble(LiczbaDruga.Text);
+                int dzialanie = Convert.ToInt32(Dzialanie.SelectedValue);
+                // Wybór rodzaju działania
+                switch (dzialanie)
+                {
+                    case 0:
+                        double wynik = (num1 + num2);
+                        Wynik.Text = wynik.ToString();
+                        dodajDzialanie(LiczbaPierwsza.Text, LiczbaDruga.Text, dzialanie, wynik.ToString());
+                        break;
+
+                    case 1:
+                        wynik = (num1 - num2);
+                        Wynik.Text = wynik.ToString();
+                        dodajDzialanie(LiczbaPierwsza.Text, LiczbaDruga.Text, dzialanie, wynik.ToString());
+                        break;
+                    case 2:
+                        wynik = (num1 * num2);
+                        Wynik.Text = wynik.ToString();
+                        dodajDzialanie(LiczbaPierwsza.Text, LiczbaDruga.Text, dzialanie, wynik.ToString());
+                        break;
+                    case 3:
+                        if (num2 == 0)
+                        {
+                            Wynik.Text = "Nie dzielimy przez 0";
+                        }
+                        else
+                        {
+                            wynik = (num1 / num2);
+                            Wynik.Text = wynik.ToString();
+                            dodajDzialanie(LiczbaPierwsza.Text, LiczbaDruga.Text, dzialanie, wynik.ToString());
+                        }
+                        break;
+                    default:
+                        Wynik.Text = "Coś tam";
+                        break;
+                }
+            }
+            // Obsługa wyjątków (dzielenie przez zero)
+            catch (Exception ex)
+            {
+                Wynik.Text = "Błąd formatu wprowadzonej liczby!";
+            }
+
+
+
+        }
+        // Metoda odtwarzająca działanie z historii działań
+        protected void Odtworz_Click(object sender, EventArgs e)
+        {
+            string opisDzialania = Historia.SelectedValue;
+            string[] pola = opisDzialania.Split(';');
+
+            LiczbaPierwsza.Text = pola[0];
+            LiczbaDruga.Text = pola[1];
+            Dzialanie.Text = pola[2];
+            Wynik.Text = pola[3];
+        }
+
+       
     }
 }
